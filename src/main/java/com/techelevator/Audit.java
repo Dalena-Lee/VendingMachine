7827d06dbbc073d227ae8046870375e648dd0c71
Dalena Nguyen <dalenan330@gmail.com>
2022-11-17 22:08:16 -0600

    Created new methods to set the file paths

--- src/main/java/com/techelevator/Audit.java
+++ src/main/java/com/techelevator/Audit.java
@@ -1,4 +1,28 @@
 package com.techelevator;
 
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Audit extends Purchase{
+    String audit;
+    File auditFile;
+    LocalDateTime dateTime;
+
+    public Audit(String audit) {
+        this.audit = audit;
+    }
+
+    public void setAuditFile(File auditFile) {
+        this.auditFile = auditFile;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 }
