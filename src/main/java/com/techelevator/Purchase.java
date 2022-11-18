5dde42d12f31d5b351cd2cc5e75d026e41a766c6
Kyoko Inagawa <kyokoinagawa9@gmail.com>
2022-11-18 12:00:17 -0800

    m

--- src/main/java/com/techelevator/Purchase.java
+++ src/main/java/com/techelevator/Purchase.java
@@ -1,61 +1,15 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 public class Purchase {
-<<<<<<< HEAD
-//    private BigDecimal purchasePrice;
-//    private BigDecimal currentBalance;
-//    private String selectedItem;
-//
-//    private BigDecimal nickels;
-//    private BigDecimal dimes;
-//    private BigDecimal quarters;
-//
-//    private int oneDollarBill;
-//    private int fiveDollarBill;
-//    private int tenDollarBill;
-//    private int twentyDollarBill;
-//
-//
-//    //getters and setters
-//    public String getSelectedItem() {
-//        return selectedItem;
-//    }
-//
-//    public void setSelectedItem(String selectedItem) {
-//        this.selectedItem = selectedItem;
-//    }
-//
-//
-//    public static BigDecimal calculateChange(BigDecimal purchasePrice, String selectedItem,  ){
-//
-//    }
-//
-//    //boolean isBOGODO();
-//    public static Boolean isBOGODO(){
-//
-//        public class CurrentDateTimeExample2 {
-//            public static void main(String[] args) {
-//                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-//                Date date = new Date();
-//                System.out.println(formatter.format(date));
-//            }
-//        }
-//    }
-//    //getCurrentBalance(); //purchasePrice, selectedItem,
-//    //boolean isThanksgiving; get local date and time
-//    int purchasesUntilBOGODO;
-=======
-    private BigDecimal purchasePrice;
+
     private BigDecimal currentBalance;
-    private String selectedItem;
 
     private BigDecimal nickels;
     private BigDecimal dimes;
@@ -70,13 +24,6 @@ public class Purchase {
     }
 
     //getters and setters
-    public String getSelectedItem() {
-        return selectedItem;
-    }
-
-    public void setSelectedItem(String selectedItem) {
-        this.selectedItem = selectedItem;
-    }
 
     public BigDecimal getCurrentBalance() {
         return currentBalance;
@@ -86,26 +33,44 @@ public class Purchase {
         this.currentBalance = currentBalance;
     }
 
-    public BigDecimal calculateChange(){
+    public BigDecimal calculateChange(BigDecimal purchasePrice){
         currentBalance -= purchasePrice;
         // if bogodo is true, buy one, get second off for a dollar, resets after bogodo price
         return currentBalance;
     }
 
-    public Boolean isBOGODO(){
+    public boolean isBOGODO(){
      // get date of first nov of current year
         // if 11/1 is thurs, add three weeks
         // if not, find first thursday and then add three weeks
         // if current day is thanksgiving , then return true bogodo
-        LocalDate firstNov;
-        if (fir){
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
 
-     }
+        //gives first of nov of current year
+        LocalDate firstNov = LocalDate.of(year, 11, 1);
+        int dayOfThanks = 0;
+
+        // if first day of nov is thursday:
+        if (firstNov.getDayOfWeek().equals(DayOfWeek.THURSDAY)){
+
+            //setting thanksgiving day
+             dayOfThanks = firstNov.plusWeeks(3).getDayOfMonth();
+            LocalDate thanksgiving = LocalDate.of(year, 11, dayOfThanks);
+                if (now.equals(thanksgiving)){
+                    return true;
+                } else {
+                    return false;
+                }
+
+                //first day of nov isn't thurs
+        } else {
+           dayOfThanks = firstNov.with(TemporalAdjusters.next(DayOfWeek.THURSDAY)).plusWeeks(3).getDayOfMonth();
+           //setting day of thanks
+            LocalDate thanksgiving = LocalDate.of(year, 11, dayOfThanks);
+            return true;
+        }
     }
-    //getCurrentBalance(); //purchasePrice, selectedItem,
-    //boolean isThanksgiving; get local date and time
-    int purchasesUntilBOGODO;
 
 
->>>>>>> 8eee546e0bc9df5ff3d2bf4f5b73be768b287243
 }
